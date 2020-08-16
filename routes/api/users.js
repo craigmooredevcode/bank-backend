effbbf2b3c03312014d6a1a428fdd91d37b6ed6d
Richard Emate <richardemate@gmail.com>
2020-08-16 10:53:10 +0100

    added trx failed modal msg

--- routes/api/users.js
+++ routes/api/users.js
@@ -160,7 +160,7 @@ router.post("/login", (req, res) => {
             payload,
             keys.secretOrKey,
             {
-              expiresIn: 31556926 // 1 year in seconds
+              expiresIn: 240// 1 in seconds
             },
             (err, token) => {
               res.json({
